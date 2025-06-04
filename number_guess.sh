9a4328b67413a2f522716ba00d5613d9595c34b1
Joana dos Santos Raposo <amarelaxx@gmail.com>
2025-06-04 01:06:51 +0000

    FEAT: backup from database

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#Definindo o BD:
+PSQL="psql --username=postgres --dbname=number_guess"
+
+echo "=== GERADOR DE NÚMEROS ALEATÓRIOS ==="
+echo
+_numero=$((RANDOM % 1000 + 1))
+echo " Resultado = $numero"
