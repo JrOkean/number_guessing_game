6757f6269bd755f4872337869cc47f5e0ee0a2c2
Joana dos Santos Raposo <amarelaxx@gmail.com>
2025-06-04 02:11:12 +0000

    FIXED: backup database now is correct; ADD: integration with the database and skeleton of the script

--- number_guess.sh
+++ number_guess.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 #Definindo o BD:
-PSQL="psql --username=postgres --dbname=number_guess"
+PSQL="psql --username=postgres --dbname=number_guess -t --no-align -c"
 
 echo "=== GERADOR DE NÚMEROS ALEATÓRIOS ==="
 echo
-_numero=$((RANDOM % 1000 + 1))
-echo " Resultado = $numero"
+
+#Começo do jogo, escolha de jogador 
+#(nome do jogador)
+echo "Informe seu nome, jogador"
+read NAMEPlayer
+
+#Verificar se o jogador já é cadastrado (verificação no banco de dados)
+USER_INFO=$($PSQL "SELECT playername, games_played, best_game FROM players WHERE playername='$NAMEPlayer'")
+
+#Se USER_INFO não tem leght zero. Se estiver vazia, então é a primeira vez do jogador. Do contrário, ele já efetuou pelo menos uma jogada.
+if [[ -z $USER_INFO ]]; then
+  echo "Bem-vindo ao jogo, $NAMEPlayer, é um prazer tê-lo aqui!"
+  INSERT_USER=$($PSQL "INSERT INTO players(playername) VALUES('$NAMEPlayer')")
+else 
+  "$USER_INFO" | while IFS="|" read NAME GAMES_PLAYED BEST_GAME; do
+    echo "Olha ele aí, $NAME! Você já participou de $GAMES_PLAYED jogos e seu record foi a aposta no jogo $BEST_GAME."
+  done
+fi
+
+#Gere um número aleatório
+SECRET_NUMBER=$((RANDOM % 1000 + 1))
+echo "Vamos ver como está sua sorte!"
+echo "Aposte em um número de 1 a 1000"
+read P_GUESS
+
+
---
+++ number_guess.sql
@@ -0,0 +1,100 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: players; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.players (
+    player_id integer NOT NULL,
+    playername character varying(22) NOT NULL,
+    games_played integer DEFAULT 0,
+    best_game integer
+);
+
+
+ALTER TABLE public.players OWNER TO postgres;
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.players_player_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.players_player_id_seq OWNER TO postgres;
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.players_player_id_seq OWNED BY public.players.player_id;
+
+
+--
+-- Name: players player_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.players ALTER COLUMN player_id SET DEFAULT nextval('public.players_player_id_seq'::regclass);
+
+
+--
+-- Data for Name: players; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.players (player_id, playername, games_played, best_game) FROM stdin;
+\.
+
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.players_player_id_seq', 1, false);
+
+
+--
+-- Name: players players_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.players
+    ADD CONSTRAINT players_pkey PRIMARY KEY (player_id);
+
+
+--
+-- Name: players players_playername_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.players
+    ADD CONSTRAINT players_playername_key UNIQUE (playername);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
